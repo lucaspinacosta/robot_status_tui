02d5e3d81e07857bb24d9e559cdcfad3b6ab68fa
Lucas Costa <lucaspinacosta94@gmail.com>
2025-02-01 22:53:27 +0000

    Initialize tmux session with ROS environment setup for robot status TUI

--- scripts/start_tmux_session.sh
+++ scripts/start_tmux_session.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 tmux new-session -d -s robot_status
 tmux split-window -v
+tmux source ~/workspace/fortis_ws/devel/setup.bash
 tmux send-keys -t 0 "roslaunch robot_status_tui robot_status.launch" C-m
 tmux attach-session -t robot_status
